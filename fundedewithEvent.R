360c099b4c27c301da5b657704972b618d30d49b
sfahmadi <95769960+sfahmadi@users.noreply.github.com>
2022-01-26 09:06:52 -0500

    Delete fundedewithEvent.R

--- fundedewithEvent.R
+++
@@ -1,8 +0,0 @@
-fundedewithEvent<-function(states, fulltimes,truepar,namesyout,eventdata){
-	truepar["starttime"]<-unclass(as.POSIXct(strptime(date(),"%c")))[1]
-try({out <- dede(states, fulltimes, "derivs", parms=truepar, dllname="delaymymod",initfunc="initmod", nout=33, rtol=1e-14, atol=1e-14, method="adams",n_history = 100000,events=list(data=eventdata))});
-colnames(out)[(length(states)+2):(length(states)+length(namesyout)+1)]=namesyout
-
-
-out
-}
